ca2e996862b175e69f439aa4379c8d092ececd64
jstapleton463 <42473264+jstapleton463@users.noreply.github.com>
2020-03-31 20:08:53 -0400

    Create challenge-1.py

---
+++ challenge-1.py
@@ -0,0 +1,8 @@
+# Change the code below to create a program that prints out all the numbers between one and ten.
+
+# Beginner:     10 lines
+# Intermediate: 3 lines
+# Advanced:     2 lines
+
+i = 1
+print(i)
