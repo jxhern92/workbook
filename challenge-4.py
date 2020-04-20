a0d0a38d159936d937106986071f7e1f1f49c567
Gustavo7892 <60274779+Gustavo7892@users.noreply.github.com>
2020-04-20 18:56:05 -0400

    Update challenge-4.py

--- challenge-4.py
+++ challenge-4.py
@@ -3,9 +3,18 @@ from random import randint
 rand_num = randint(0, 100)
 count = 0
 
-while count < 5:
-  print("What is your name?")
-  input()
+while count < 100:
+  print("guess a number between 1-100")
+  m = input()
+  n = int(m)
+  if rand_num < n:
+    print ("your guess is too high")
+  if rand_num > n:
+    print ("your guess is too low")
   count = count + 1
+  if n == rand_num:
+    count = 100
+
+
 
 print("All done!")
